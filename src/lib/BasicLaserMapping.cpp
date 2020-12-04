7fc90acb6116bf565559eca41d49e7b2d1592f0a
Janarth Dheenadhayalan <jdheenad@andrew.cmu.edu>
2020-12-03 22:08:56 -0500

    small optimizations to for loops

--- src/lib/BasicLaserMapping.cpp
+++ src/lib/BasicLaserMapping.cpp
@@ -428,77 +428,87 @@ bool BasicLaserMapping::process(Time const& laserOdometryTime)
    }
    _laserCloudValidInd.clear();
    _laserCloudSurroundInd.clear();
+
+   size_t k_offset = _laserCloudWidth * _laserCloudHeight;
+
    for (int i = centerCubeI - 2; i <= centerCubeI + 2; i++)
    {
-      for (int j = centerCubeJ - 2; j <= centerCubeJ + 2; j++)
+
+      if (i >= 0 && i < _laserCloudWidth)
       {
-         for (int k = centerCubeK - 2; k <= centerCubeK + 2; k++)
+         float centerX = 50.0f * (i - _laserCloudCenWidth);
+
+         for (int j = centerCubeJ - 2; j <= centerCubeJ + 2; j++)
          {
-            if (i >= 0 && i < _laserCloudWidth &&
-                j >= 0 && j < _laserCloudHeight &&
-                k >= 0 && k < _laserCloudDepth)
+
+            if (j >= 0 && j < _laserCloudHeight)
             {
-
-               float centerX = 50.0f * (i - _laserCloudCenWidth);
                float centerY = 50.0f * (j - _laserCloudCenHeight);
-               float centerZ = 50.0f * (k - _laserCloudCenDepth);
+               size_t j_offset = _laserCloudWidth * j;
 
-               pcl::PointXYZI transform_pos = (pcl::PointXYZI) _transformTobeMapped.pos;
-
-               bool isInLaserFOV = false;
-               for (int ii = -1; ii <= 1; ii += 2)
+               for (int k = centerCubeK - 2; k <= centerCubeK + 2; k++)
                {
-                  for (int jj = -1; jj <= 1; jj += 2)
+                  if (k >= 0 && k < _laserCloudDepth)
                   {
-                     for (int kk = -1; kk <= 1; kk += 2)
+                     float centerZ = 50.0f * (k - _laserCloudCenDepth);
+
+                     pcl::PointXYZI transform_pos = (pcl::PointXYZI)_transformTobeMapped.pos;
+
+                     bool isInLaserFOV = false;
+                     pcl::PointXYZI corner;
+                     for (int ii = -1; ii <= 1 && !isInLaserFOV; ii += 2)
                      {
-                        pcl::PointXYZI corner;
                         corner.x = centerX + 25.0f * ii;
-                        corner.y = centerY + 25.0f * jj;
-                        corner.z = centerZ + 25.0f * kk;
 
-                        float squaredSide1 = calcSquaredDiff(transform_pos, corner);
-                        float squaredSide2 = calcSquaredDiff(pointOnYAxis, corner);
-
-                        float check1 = 100.0f + squaredSide1 - squaredSide2
-                           - 10.0f * sqrt(3.0f) * sqrt(squaredSide1);
-
-                        float check2 = 100.0f + squaredSide1 - squaredSide2
-                           + 10.0f * sqrt(3.0f) * sqrt(squaredSide1);
-
-                        if (check1 < 0 && check2 > 0)
+                        for (int jj = -1; jj <= 1 && !isInLaserFOV; jj += 2)
                         {
-                           isInLaserFOV = true;
+                           corner.y = centerY + 25.0f * jj;
+
+                           for (int kk = -1; kk <= 1 && !isInLaserFOV; kk += 2)
+                           {
+                              corner.z = centerZ + 25.0f * kk;
+
+                              float squaredSide1 = calcSquaredDiff(transform_pos, corner);
+                              float squaredSide2 = calcSquaredDiff(pointOnYAxis, corner);
+
+                              float val1 = 100.0f + squaredSide1 - squaredSide2;
+                              float val2 = 10.0f * sqrt(3.0f) * sqrt(squaredSide1);
+
+                              if (((val1 - val2) < 0) && ((val1 + val2) > 0))
+                              {
+                                 isInLaserFOV = true;
+                              }
+                           }
                         }
                      }
-                  }
-               }
 
-               size_t cubeIdx = i + _laserCloudWidth * j + _laserCloudWidth * _laserCloudHeight * k;
-               if (isInLaserFOV)
-               {
-                  _laserCloudValidInd.push_back(cubeIdx);
+                     size_t cubeIdx = i + j_offset + k_offset * k;
+                     if (isInLaserFOV)
+                     {
+                        _laserCloudValidInd.push_back(cubeIdx);
+                     }
+                     _laserCloudSurroundInd.push_back(cubeIdx);
+                  } // k_true
                }
-               _laserCloudSurroundInd.push_back(cubeIdx);
-            }
+            } // j_true
          }
-      }
+      } // i_true
    }
 
    // prepare valid map corner and surface cloud for pose optimization
    _laserCloudCornerFromMap->clear();
    _laserCloudSurfFromMap->clear();
-   for (auto const& ind : _laserCloudValidInd)
+   for (auto const &ind : _laserCloudValidInd)
    {
       *_laserCloudCornerFromMap += *_laserCloudCornerArray[ind];
       *_laserCloudSurfFromMap += *_laserCloudSurfArray[ind];
    }
 
    // prepare feature stack clouds for pose optimization
-   for (auto& pt : *_laserCloudCornerStack)
+   for (auto &pt : *_laserCloudCornerStack)
       pointAssociateTobeMapped(pt, pt);
 
-   for (auto& pt : *_laserCloudSurfStack)
+   for (auto &pt : *_laserCloudSurfStack)
       pointAssociateTobeMapped(pt, pt);
 
    // down sample feature stack clouds
@@ -524,18 +534,28 @@ bool BasicLaserMapping::process(Time const& laserOdometryTime)
       pointAssociateToMap(_laserCloudCornerStackDS->points[i], pointSel);
 
       int cubeI = int((pointSel.x + CUBE_HALF) / CUBE_SIZE) + _laserCloudCenWidth;
-      int cubeJ = int((pointSel.y + CUBE_HALF) / CUBE_SIZE) + _laserCloudCenHeight;
-      int cubeK = int((pointSel.z + CUBE_HALF) / CUBE_SIZE) + _laserCloudCenDepth;
-
-      if (pointSel.x + CUBE_HALF < 0) cubeI--;
-      if (pointSel.y + CUBE_HALF < 0) cubeJ--;
-      if (pointSel.z + CUBE_HALF < 0) cubeK--;
-
-      if (cubeI >= 0 && cubeI < _laserCloudWidth &&
-          cubeJ >= 0 && cubeJ < _laserCloudHeight &&
-          cubeK >= 0 && cubeK < _laserCloudDepth)
+      if (pointSel.x + CUBE_HALF < 0)
+         cubeI--;
+      if (!(cubeI >= 0 && cubeI < _laserCloudWidth))
       {
-         size_t cubeInd = cubeI + _laserCloudWidth * cubeJ + _laserCloudWidth * _laserCloudHeight * cubeK;
+         continue;
+      }
+
+      int cubeJ = int((pointSel.y + CUBE_HALF) / CUBE_SIZE) + _laserCloudCenHeight;
+      if (pointSel.y + CUBE_HALF < 0)
+         cubeJ--;
+      if (!(cubeJ >= 0 && cubeJ < _laserCloudHeight))
+      {
+         continue;
+      }
+
+      int cubeK = int((pointSel.z + CUBE_HALF) / CUBE_SIZE) + _laserCloudCenDepth;
+      if (pointSel.z + CUBE_HALF < 0)
+         cubeK--;
+      if (cubeK >= 0 && cubeK < _laserCloudDepth)
+      {
+
+         size_t cubeInd = cubeI + _laserCloudWidth * cubeJ + k_offset * cubeK;
          _laserCloudCornerArray[cubeInd]->push_back(pointSel);
       }
    }
@@ -546,24 +566,33 @@ bool BasicLaserMapping::process(Time const& laserOdometryTime)
       pointAssociateToMap(_laserCloudSurfStackDS->points[i], pointSel);
 
       int cubeI = int((pointSel.x + CUBE_HALF) / CUBE_SIZE) + _laserCloudCenWidth;
-      int cubeJ = int((pointSel.y + CUBE_HALF) / CUBE_SIZE) + _laserCloudCenHeight;
-      int cubeK = int((pointSel.z + CUBE_HALF) / CUBE_SIZE) + _laserCloudCenDepth;
-
-      if (pointSel.x + CUBE_HALF < 0) cubeI--;
-      if (pointSel.y + CUBE_HALF < 0) cubeJ--;
-      if (pointSel.z + CUBE_HALF < 0) cubeK--;
-
-      if (cubeI >= 0 && cubeI < _laserCloudWidth &&
-          cubeJ >= 0 && cubeJ < _laserCloudHeight &&
-          cubeK >= 0 && cubeK < _laserCloudDepth)
+      if (pointSel.x + CUBE_HALF < 0)
+         cubeI--;
+      if (!(cubeI >= 0 && cubeI < _laserCloudWidth))
       {
-         size_t cubeInd = cubeI + _laserCloudWidth * cubeJ + _laserCloudWidth * _laserCloudHeight * cubeK;
+         continue;
+      }
+
+      int cubeJ = int((pointSel.y + CUBE_HALF) / CUBE_SIZE) + _laserCloudCenHeight;
+      if (pointSel.y + CUBE_HALF < 0)
+         cubeJ--;
+      if (!(cubeJ >= 0 && cubeJ < _laserCloudHeight))
+      {
+         continue
+      }
+
+      int cubeK = int((pointSel.z + CUBE_HALF) / CUBE_SIZE) + _laserCloudCenDepth;
+      if (pointSel.z + CUBE_HALF < 0)
+         cubeK--;
+      if (cubeK >= 0 && cubeK < _laserCloudDepth)
+      {
+         size_t cubeInd = cubeI + _laserCloudWidth * cubeJ + k_offset * cubeK;
          _laserCloudSurfArray[cubeInd]->push_back(pointSel);
       }
    }
 
    // down size all valid (within field of view) feature cube clouds
-   for (auto const& ind : _laserCloudValidInd)
+   for (auto const &ind : _laserCloudValidInd)
    {
       _laserCloudCornerDSArray[ind]->clear();
       _downSizeFilterCorner.setInputCloud(_laserCloudCornerArray[ind]);
